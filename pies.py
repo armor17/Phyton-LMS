46d3089fc3e5c484c93589e9aceaf62bc9ba2268
armor17 <amorusencev@gmail.com>
2026-03-19 00:12:52 +0300

    Create pies.py

---
+++ pies.py
@@ -0,0 +1,6 @@
+grandmother=1
+num_of_pies=int(input())
+num_of_grandchildren=int(input())
+num_of_consumers=num_of_grandchildren+grandmother
+print(num_of_pies//num_of_consumers)
+print(num_of_pies%num_of_consumers)
