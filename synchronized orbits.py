ea3ea0f4eb04969d420c642588f1987e6ad319c2
armor17 <amorusencev@gmail.com>
2026-03-26 22:06:01 +0300

    Create synchronized orbits.py

---
+++ synchronized orbits.py
@@ -0,0 +1,19 @@
+x = int(input())
+if x != 9 and x % 9 == 0:
+    print(9)
+elif x !=8 and x % 8 == 0:
+    print(8)
+elif x !=7 and x % 7 == 0:
+    print(7)
+elif x != 6 and x % 6 == 0:
+    print(6)
+elif x != 5 and x % 5 == 0:
+    print(5)
+elif x != 4 and x % 4 == 0:
+    print(4)
+elif x != 3 and x % 3 == 0:
+    print(3)
+elif x != 2 and x % 2 == 0:
+    print(2)
+else:
+    print(-1)
